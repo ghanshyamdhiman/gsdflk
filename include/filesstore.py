f7c11593be08eb04f8644ea2fb435d44526af554
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2021-01-07 23:59:49 +0530

    Update filesstore.py

--- include/filesstore.py
+++ include/filesstore.py
@@ -1,10 +1,18 @@
-def store_the_file(file_name):
+import webscrapper
+
+the_url = "http://www.values.com/inspirational-quotes"
+
+the_scrapper = LyricsScrapper(the_url)
+
+the_lyrics_data = the_scapper.get_lyics()
+
+def store_the_file(file_name, lyrics_data):
     try:
        f = open(file_name, "a")
-       # perform file operations
+       f.write(lyrics_data)
     finally:
        f.close()
    
 the_file_name = "test_file.txt"
 
-store_the_file(the_file_name)
+store_the_file(the_file_name, the_lyrics_data)
