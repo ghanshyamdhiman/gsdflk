7632e46dcf30c5eba6f8a5530d2615d844e0658d
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2021-01-07 21:42:11 +0530

    Update filesstore.py

--- include/filesstore.py
+++ include/filesstore.py
@@ -1,9 +1,9 @@
 def store_the_file(file_name):
-try:
-   f = open(file_name, mode= 'a' encoding = 'utf-8')
-   # perform file operations
-finally:
-   f.close()
+    try:
+       f = open(file_name, mode= 'a' encoding = 'utf-8')
+       # perform file operations
+    finally:
+       f.close()
    
 the_file_name = "test_file.txt"
 
