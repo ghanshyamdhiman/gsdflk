503859d2091f8096e453cc435bc29875151d148d
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2021-01-08 00:21:14 +0530

    Update webscrapper.py

--- include/webscrapper.py
+++ include/webscrapper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 class LyricsScrapper:
   def __init__(self, website_url):
-    self.weburl = website_ul
+    self.weburl = website_url
     
   def get_lyrics(self):
     r = requests.get(URL)   
